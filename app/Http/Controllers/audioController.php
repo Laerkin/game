6e015a6d53b08ca21cf94865d68c470d08d55377
wax46 <wax46@hotmail.fr>
2018-05-18 15:51:36 +0200

    supp commentaires
    F

--- app/Http/Controllers/audioController.php
+++ app/Http/Controllers/audioController.php
@@ -23,24 +23,10 @@ class audioController extends Controller
         $file = $values['audio'];
         //dd($file->getRealPath());
 
-        // Affiche le nom du fichier envoyé
-       // Name : dd($file->getClientOriginalName());
 
-        // Affiche l'extension du fichier envoyé
-        // Extension : dd($file->getClientOriginalExtension());
-
-        // Stock le fichier envoyé dans storage avec un nom aléatoire
-        // $file->store('public');
-
-
-        // Stock avec un autre nom
         $ext = strtolower($file->getClientOriginalExtension());
         $file->storeAs('public/audio',str_random(8).$ext);
 
-        //$file->storeAs('public', 'dd.' . strtolower($file->getClientOriginalExtension()));
-
-        //Déplacement dans le bon dossier
-        //$file->move(public_path('/compagnies/'), 'toto.' . $ext);
         return Redirect::back();
 
     }
--- app/Http/Controllers/videoController.php
+++ app/Http/Controllers/videoController.php
@@ -24,24 +24,10 @@ class videoController extends Controller
         $file = $values['video'];
         //dd($file->getRealPath());
 
-        // Affiche le nom du fichier envoyé
-        // Name : dd($file->getClientOriginalName());
-
-        // Affiche l'extension du fichier envoyé
-        // Extension : dd($file->getClientOriginalExtension());
-
-        // Stock le fichier envoyé dans storage avec un nom aléatoire
-        // $file->store('public');
-
-
-        // Stock avec un autre nom
         $ext = strtolower($file->getClientOriginalExtension());
         $file->storeAs('public/video', str_random(8) .$ext);
 
-        //$file->storeAs('public', 'dd.' . strtolower($file->getClientOriginalExtension()));
-
-        // Déplacement dans le bon dossier
-        // $file->move(public_path('/compagnies/'), 'toto.' . $ext);
+      
         return Redirect::back();
 
     }
