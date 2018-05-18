025cc5c4ddb06af9539dc05b973264f76057730c
Saltyapkallu <guillaume.bourgeot@sfr.fr>
2018-05-18 16:36:46 +0200

    MAJ interface
    MAJ de l'interface editeur (ui-editor-(main))

--- resources/views/ui-editor-(main)/index.blade.php
+++ resources/views/ui-editor-(main)/index.blade.php
@@ -2,7 +2,7 @@
  
   @section('content')
 
-    <div class="p-0" id="editor-tab">
+  <div class="p-0" id="editor-tab">
     <div class="container-fluid">
       <div class="row w-100" id="tabs">
         <div class="col-sm-2 col-md-3 col-lg-2 col-xl-2 col-3" id="story">
@@ -20,31 +20,25 @@
       </div>
     </div>
   </div>
-  <div class="p-0" id="editor-tab">
-    <div class="container-fluid my-1">
-      <div class="row w-100" id="outils"></div>
-    </div>
-  </div>
-  <div class="p-0" id="editor-tab">
-    <div class="container-fluid my-1">
-      <div class="row w-100" id="current"></div>
-    </div>
-  </div>
   <div class="" id="editor">
     <div class="container-fluid m-2">
       <div class="row m-1">
-        <div class="flex-column h-100 col-sm-2 col-md-2 col-2" id="thumbnails">
+        <div class="flex-column h-100 col-md-2" id="thumbnails" >
           <div class="m-1 align-items-center align-self-center" id="thumbnail"></div>
           <div class="m-1 align-items-center align-self-center" id="thumbnail"></div>
           <div class="m-1 align-items-center align-self-center" id="thumbnail"></div>
           <div class="m-1 align-items-center align-self-center" id="thumbnail"></div>
         </div>
-        <div class="col-md-4 flex-column col-sm-4 col-4" id="tools">
-          <div class="row h-50 my-1"></div>
-          <div class="row h-50 my-1"></div>
-        </div>
-        <div class="col-md-6 flex-column align-self-center" id="previews" >
-          <div class="row h-75 align-items-center align-self-center"></div>
+        <div class="col-md-10 my-1">
+          <div class="row my-1"></div>
+          <div class="row my-1"></div>
+          <div class="row">
+            <div class="col-md-4 h-100 flex-column">
+              <div class="div h-50 m-1"></div>
+              <div class="div h-50 m-1"></div>
+            </div>
+            <div class="col-md-8 p-2"></div>
+          </div>
         </div>
       </div>
     </div>
