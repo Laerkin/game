793a80cc441d3de7adf9a4b3520c7fb454faff2f
wax46 <wax46@hotmail.fr>
2018-05-22 13:04:27 +0200

    d
    d

--- app/Http/Controllers/profilController.php
+++ app/Http/Controllers/profilController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class profilController extends Controller
-    {
+{
 
-        public function show($id)
-        {
-            return view('profil.index', ['user' => users::findOrFail($id)]);
-        }
+    public function show($id)
+    {
+        return view('profil.index', ['user' => users::findOrFail($id)]);
     }
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+}
