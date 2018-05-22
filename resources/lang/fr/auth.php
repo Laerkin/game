d954215b0cf3a065348249f848d59932b7d7d6f0
Damien Vallier <37544705+MsieurDam@users.noreply.github.com>
2018-05-22 11:22:27 +0200

    Design V0.1
    Page edit en place

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'username' => 'Nom d\'utilisateur',
+    'email' => 'Adresse email',
+    'password' => 'Mot de passe',
+    'confirmPassword' => 'Confirmez le mot de passe',
+    'register' => 'Inscription',
+    'submitRegister' => 'S\'enregistrer',
+    'submitLogin' => 'Se connecter',
+    'forgotPassword' => 'Mot de passe oublié?',
+    'rememberMe' => 'se souvenir de moi',
+    'login' => 'Connexion',
+    'resetPassword' => 'Mot de passe oublié',
+    'resetMail' => 'Envoyer le courrier de récupération',
+];
