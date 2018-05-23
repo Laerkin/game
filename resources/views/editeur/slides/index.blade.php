1851109e93eb8a6106f28c793f1b907bece9984a
Syn <archangekino@hotmail.com>
2018-05-23 11:14:52 +0200

    dz
    dza

--- resources/views/editeur/slides/index.blade.php
+++ resources/views/editeur/slides/index.blade.php
@@ -1,37 +1,49 @@
-
-<style>
-    *{
-        border:1px solid red;
-    }
-</style>
-<div class="" id="editor">
-    <div class="container-fluid m-2">
-        <div class="row m-1">
-            <div class="col-md-6">
-            <div class="flex-column h-100 col-sm-2 col-md-2 col-2" id="thumbnails">
-                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
-                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
-                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
-                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="slideItem">
+                slide #
             </div>
-            <div class="col-md-4 col-sm-4 col-4" id="tools">
-                <div class="h-50 my-1">tools</div>
-                <div class="h-50 my-1">tools</div>
+            <div class="slideItem">
+                slide #
             </div>
-
+            <div class="slideItem">
+                slide #
             </div>
-            <div class="col-md-6">
-
-
-
-
-                            gros cul
-
-
-
-
-
+            <div class="slideItem">
+                slide #
+            </div>
+            <div class="slideItem">
+                slide #
+            </div>
+            <div class="slideItem">
+                slide #
+            </div>
+            <div class="slideItem">
+                slide #
+            </div>
+            <div class="slideItem">
+                slide #
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-12">lstElements</div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">Element options</div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-12">--- LIGNE OUTILS ---</div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">/* Preview */</div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">/* Localisations */</div>
+            </div>
         </div>
     </div>
+</div>
