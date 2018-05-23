248b9d35f480c93f0b6afa73168466d0956a08d4
Damien Vallier <37544705+MsieurDam@users.noreply.github.com>
2018-05-23 10:04:46 +0200

    V 20180523_1

--- resources/views/personnage/index.blade.php
+++ resources/views/personnage/index.blade.php
@@ -1,16 +1,49 @@
 @extends('layouts.master')
 
 @section('content')
+    <!--
+    Feuille de style
+    -->
+    <style>
 
+        body{
+            background-color:#DFF2FF;
+            color: #856D4D;
+            font-family: 'Indie Flower', cursive;
+            font-weight: bold;
+        }
+        label{
+            font-size: 20px;
+        }
+
+        /** Responsive */
+        @media all and (max-width: 900px)
+        {
+            label{
+                font-size: 18px;
+            }
+        }
+
+    </style>
+
+<div class="container-fluid">
     <form method="post" enctype="multipart/form-data" style="text-align: center;">
         @csrf
-
-        <div class="input-group mb-3">
-            <label for="name">Nom du personnage :<br></label>
-            <input type="text" class="form-control" id="name" placeholder="Nom du perso">
+        <div class="form-row">
+            <div class="form-group col-md-3 col-md-offset-5">
+                <div class="input-group mb-3">
+                    <label for="name">Nom du personnage :<br></label>
+                </div>
+                <div class="form-group col-md-12">
+                    <input type="text" class="form-control" id="name" placeholder="Nom du perso">
+                </div>
+            </div>
         </div>
+
         <br>
+
         <label for="img">Choisir un fichier pour le personnage (taille carré)<br></label>
+        <br>
         <input type="file" id="img" name="img_personnage" accept="image/*" onchange="loadFile(event)">
         <br>
         <br>
@@ -26,6 +59,8 @@
             <button type="submit" value="submit">Envoyer</button>
         </div>
     </form>
+</div>
+    <br>
 
     <script>
         var loadFile = function(event) {
