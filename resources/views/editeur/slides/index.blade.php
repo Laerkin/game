5b01b0225e862517d84165e3af664ada16064603
Syn <archangekino@hotmail.com>
2018-05-23 10:49:24 +0200

    dd

--- resources/views/editeur/slides/index.blade.php
+++ resources/views/editeur/slides/index.blade.php
@@ -1,1 +1,37 @@
-Slides content
+
+<style>
+    *{
+        border:1px solid red;
+    }
+</style>
+<div class="" id="editor">
+    <div class="container-fluid m-2">
+        <div class="row m-1">
+            <div class="col-md-6">
+            <div class="flex-column h-100 col-sm-2 col-md-2 col-2" id="thumbnails">
+                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
+                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
+                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
+                <div class="m-1 align-items-center align-self-center" id="thumbnail">thumbnail</div>
+            </div>
+            <div class="col-md-4 col-sm-4 col-4" id="tools">
+                <div class="h-50 my-1">tools</div>
+                <div class="h-50 my-1">tools</div>
+            </div>
+
+            </div>
+            <div class="col-md-6">
+
+
+
+
+                            gros cul
+
+
+
+
+
+            </div>
+        </div>
+        </div>
+    </div>
