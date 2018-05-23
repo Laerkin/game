31dfce40e7b27c6d5e8aad8b1cc23ec1e3635cd9
Damien Vallier <37544705+MsieurDam@users.noreply.github.com>
2018-05-23 17:10:05 +0200

    V20180523_7
    Design + ajout des routes pour visu sur autres pages

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,74 @@
 */
 
 
+// Routes login
 Route::auth();
 
 Route::get('/home', 'homeController@index');
-
-
+Route::post('/home', 'homeController@index');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/logout', function() {
+    Auth::logout();
+    return view('welcome');
+});
 
+
+// Routes editeur
+Route::get('edit', 'editeurController@index');
+Route::post('edit', 'editeurController@index');
+
+Route::get('/template', function () {
+    return view('template.index');
+});
+
+
+/** ROUTES AUDIO/VIDEO  **/
+Route::get('/audio', 'audioController@index');
+Route::post('/audio', 'audioController@store');
+
+Route::get('/video', 'videoController@index');
+Route::post('/video', 'videoController@store');
+
+/** EDITEUR FORMULAIRE BASIQUE */
+Route::get('/edit', function () {
+    return view('editeur.index');
+});
+
+/** EDITEUR FORMULAIRE BASIQUE */
+Route::get('/editor2', function () {
+    return view('editeur.2index');
+});
+
+// Create Story
+Route::get('/editor', 'CreateStoryController@index');
+Route::post('/editor', 'CreateStoryController@store');
+
+// Create Map
+Route::get('/map', function() {
+    return view('create_map.index');
+});
+
+
+// rich text routes
+//summernote form
+Route::get('/richtext', function () {
+    return view('richtext.index');
+});
+
+//summernote store route
+Route::post('/richtext','richtextController@store')->name('richtextPersist');
+
+//summernote display route
+Route::get('/richtext_display','richtextController@show')->name('richtextDispay');
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
 // Création de personnage
 Route::get('/personnage', function () {
     return view('personnage.index');
@@ -39,22 +96,13 @@ Route::get('/mentions_legales', function () {
 });
 
 
+
 // Template
 Route::get('/template', function () {
     return view('template.index');
 });
 
 
-
 Route::get('/personnage', 'PersonnageController@index');
 Route::post('/personnage', 'PersonnageController@store');
 
-
-
-// Create Story
-Route::get('/edit', 'CreateStoryController@index');
-Route::post('/edit', 'CreateStoryController@store');
-// Create Map
-Route::get('/map', function() {
-    return view('create_map.index');
-});
