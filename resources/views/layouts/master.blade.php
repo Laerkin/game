55d1e03d40708263973d59e7487dbc1d1c5d3bd7
Damien Vallier <37544705+MsieurDam@users.noreply.github.com>
2018-05-24 14:31:36 +0200

    Design Auth V1

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,13 @@
 
 <html lang="{{ app()->getLocale() }}">
 
+<!-- Polices -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Neuton:400,700" rel="stylesheet">
+
+<!-- CSS -->
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+
+
 <head>
 
     @yield('scripts')
