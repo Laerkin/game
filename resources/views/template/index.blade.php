5cf8a87ac1b93bfaf3bc0f7955d0eea89ddded3f
max <36971177+wax46@users.noreply.github.com>
2018-05-27 12:54:40 +0200

    lihg

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -83,13 +83,11 @@
 
 
     @section('footer')
-<<<<<<< HEAD
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         <div class="footer">
             <p>Footer</p>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-=======
         <footer>
             <div class="container-fluid b">
             <div class="row align-items-center">
@@ -122,7 +120,6 @@
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
->>>>>>> master
     @endsection
 
 
